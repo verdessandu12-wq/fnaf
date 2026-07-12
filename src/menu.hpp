4f6f9aafd90488e4fa282b313d472c0575d69578
dmn79 <mokurovdeman4@gmail.com>
2026-07-12 14:18:59 +0300

    start menu

--- src/menu.hpp
+++ src/menu.hpp
@@ -1,7 +1,18 @@
 #ifndef MENU_HPP
 #define MENU_HPP
+#include "raylib.h"
 class menu {
     public:
-
+        float width, height;
+        Texture2D background;
+        Texture2D button;
+        Rectangle Play_Butt;
+        Music backMusic;
+        void import_variable();
+        void rendering();  
+        void renderMusic();
+        bool logic();
+        void UpdateMusic();
+        void unload();
 };
 #endif
